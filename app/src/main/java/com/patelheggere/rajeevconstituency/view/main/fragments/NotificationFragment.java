41f0cf9d7a3a71005fa5a4a6cb1751305e98c2c8
vinay8287 <66133848+vinay8287@users.noreply.github.com>
2020-10-16 22:07:05 +0530

    ntg

--- app/src/main/java/com/patelheggere/rajeevconstituency/view/main/fragments/NotificationFragment.java
+++ app/src/main/java/com/patelheggere/rajeevconstituency/view/main/fragments/NotificationFragment.java
@@ -1,20 +1,40 @@
 package com.patelheggere.rajeevconstituency.view.main.fragments;
 
+import android.content.ActivityNotFoundException;
+import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.telephony.PhoneNumberUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.patelheggere.rajeevconstituency.R;
 import com.patelheggere.rajeevconstituency.base.BaseFragment;
+
+import java.util.ArrayList;
+
+import static android.app.Activity.RESULT_OK;
+import static java.security.AccessController.getContext;
 
 public class NotificationFragment extends BaseFragment {
 
     private OnFragmentInteractionListener mListener;
 
     private View mView;
+    private TextView txtSpeechInput;
+    private ImageButton btnSpeak;
+    private Button sendMesage;
+    private final int REQ_CODE_SPEECH_INPUT = 100;
 
     public NotificationFragment() {
         // Required empty public constructor
@@ -43,7 +63,59 @@ public class NotificationFragment extends BaseFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         mView =  inflater.inflate(R.layout.fragment_notification_layout, container, false);
+        txtSpeechInput = mView.findViewById(R.id.txtSpeechInput);
+        btnSpeak = mView.findViewById(R.id.btnSpeak);
+        sendMesage = mView.findViewById(R.id.send);
+
+
+        btnSpeak.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                promptSpeechInput();
+            }
+        });
+
+
+        sendMesage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PackageManager pm = mActivity.getPackageManager();
+                //openWhatsappContact("919611620128");
+                sendgroup();
+            }
+        });
         return mView;
+    }
+
+    void sendgroup()
+    {
+        String text = txtSpeechInput.getText().toString();
+        Intent sendIntent = new Intent();
+        sendIntent.setAction(Intent.ACTION_SEND);
+        sendIntent.putExtra(Intent.EXTRA_TEXT, text);
+        sendIntent.setType("text/plain");
+        sendIntent.setPackage("com.whatsapp");
+        mActivity.startActivity(sendIntent);
+    }
+    void openWhatsappContact(String number) {
+        Uri uri = Uri.parse("smsto:" + number);
+        Intent i = new Intent(Intent.ACTION_SENDTO, uri);
+        i.setPackage("com.whatsapp");
+        String text = txtSpeechInput.getText().toString();
+
+        i.putExtra(Intent.EXTRA_TEXT, text);
+        mActivity.startActivity(Intent.createChooser(i, ""));
+    }
+    void send()
+    {
+        String text = txtSpeechInput.getText().toString();
+        Intent sendIntent = new Intent("android.intent.action.MAIN");
+        sendIntent.putExtra(Intent.EXTRA_TEXT, text);
+        sendIntent.setComponent(new ComponentName("com.whatsapp", "com.whatsapp.Conversation"));
+        sendIntent.putExtra("jid", PhoneNumberUtils.stripSeparators("919611620128") + "@s.whatsapp.net");//phone number without "+" prefix
+
+        mActivity.startActivity(sendIntent);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -52,7 +124,42 @@ public class NotificationFragment extends BaseFragment {
             mListener.onFragmentInteraction(uri);
         }
     }
+    /**
+     * Showing google speech input dialog
+     * */
+    private void promptSpeechInput() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "kn-IN");
 
+        try {
+           startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+        } catch (ActivityNotFoundException a) {
+
+        }
+    }
+
+    /**
+     * Receiving speech input
+     * */
+   @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case REQ_CODE_SPEECH_INPUT: {
+                if (resultCode == RESULT_OK && null != data) {
+
+                    ArrayList<String> result = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    txtSpeechInput.setText(result.get(0));
+                }
+                break;
+            }
+
+        }
+    }
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
